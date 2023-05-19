6d3d1f2fa60ac280c82d6579c78600ca7f872bd9
aaron-five <aarongomez.guardado@gmail.com>
2023-05-19 14:30:09 -0600

    Fix: Ruta Shop/Delete

--- api/routes/shop.routes.js
+++ api/routes/shop.routes.js
@@ -5,7 +5,7 @@ router.get("/", shopController.getShops);
 router.get("/:id", shopController.getShop);
 router.post("/", shopController.createShop);
 router.put("/edit/:id", shopController.updateShop);
-router.delete("delete/:id", shopController.deleteShop);
+router.delete("/delete/:id", shopController.deleteShop);
 router.get("/:id/products", shopController.getShopAndProducts);
 
 module.exports = router;
